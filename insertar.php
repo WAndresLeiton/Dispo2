1638ccfe22915a06c53d25766678f63fe00bd566
WAndresLeiton <43972152+WAndresLeiton@users.noreply.github.com>
2018-10-08 16:49:05 -0500

    insertar.php

---
+++ insertar.php
@@ -0,0 +1,15 @@
+<?php
+    include_once("config.php");
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $id_cedulapro =$_POST["id_cedulapro"];
+        $nombre =$_POST["nombre"];
+        $apellido =$_POST["apellido"];
+        $id_cedulapro =$_POST["id_cedulapro"];
+        $id_cedulapro =$_POST["id_cedulapro"];
+        $id_cedulapro =$_POST["id_cedulapro"];
+        $id_cedulapro =$_POST["id_cedulapro"];
+        $id_cedulapro =$_POST["id_cedulapro"];
+        $id_cedulapro =$_POST["id_cedulapro"];
+
+    }
+?>
